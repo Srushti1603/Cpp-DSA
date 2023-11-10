ad5d1b4ca7817fd4c11bdd6d02d17699856682bd
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-11-10 21:25:36 +0530

    Add files via upload

---
+++ String/length.cpp
@@ -0,0 +1,16 @@
+//Findind length of string -->
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    char S[] = "Welcome";
+    for(i=0; S[i]!='\0'; i++)
+    {
+
+    }
+    cout<<"The Length Of String Is --> "<<i<<endl;
+    return 0;
+}
